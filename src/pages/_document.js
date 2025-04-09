64d97268d282ad7664fe9275992dc683eb4f73cb
Xavier, David <xdplansdev@gmail.com>
2025-04-09 08:20:01 -0300

    Push FaceXD

---
+++ src/pages/_document.js
@@ -0,0 +1,34 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+export default function Document() {
+  return (
+    <Html lang="pt-BR">
+      <Head>
+        {/* OneSignal SDK via CDN */}
+        <Script
+          src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js"
+          strategy="beforeInteractive"
+        />
+        <Script
+          id="onesignal-init"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.OneSignalDeferred = window.OneSignalDeferred || [];
+              OneSignalDeferred.push(function(OneSignal) {
+                OneSignal.init({
+                  appId: "e5258637-d4a3-4be5-8c1e-fe099d7ab13d",
+                });
+              });
+            `,
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
